4e0c87724aab9e692a3de1d90ae26e9e410aeb5a
28904 <28904@KNOBBLEBOY>
2023-08-16 05:56:59 -0600

    Agregar archivos de proyecto.

---
+++ App_Dinosaurio/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace App_Dinosaurio;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	private void BtnDatos_Clicked(object sender, EventArgs e)
+	{
+		string Dino1;
+		int Kgs;
+		int Mts;
+		double Lbs;
+		int Cms;
+
+		Dino1 = Dino.Text;
+		Kgs = Convert.ToInt32(Peso.Text);
+		Mts = Convert.ToInt32(Altura.Text);
+
+		Lbs = Kgs * 2.2;
+		Cms = Mts * 100;
+
+		LblDatos.Text = Dino1;
+		LblPeso.Text = Lbs.ToString() + "" + "Libras";
+		LblAltura.Text = Cms.ToString() + "" + "Centimetros";
+    }
+}
+
